defff5f9ef44dc4f5dbee47a583f2442bc5b04b3
sanvanvu <vuvansan961123@gmail.com>
2021-07-19 22:18:56 +0700

    create loginlayout,Get start layout, not set activity

---
+++ app/src/main/java/com/example/saqdemo/CreateAccoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.saqdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_accout);
+    }
+}
